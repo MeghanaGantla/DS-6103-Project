ca81ad693395c2ad76c033a7bfd5a742e426bb4d
MeghanaGantla <meghana_gantla@gwu.edu>
2022-04-22 02:23:45 -0400

    Update EDA and Analysis.py

--- EDA and Analysis.py
+++ EDA and Analysis.py
@@ -20,21 +20,41 @@ df.info()
 
 #%%
 # Describing data
-df.describe()
+df.describe().transpose()
 
 # %%
-# unique values in each column
+# Sorting by unique values in each column
 df.nunique().sort_values()
 
 # %%
-# checking for NA values
-df.isnull().sum()
+# Checking for null values
+print(df.isnull().sum(), "\nThere are no null values in any of the columns")
+
+#%%[markdown]
+# # Cleaning Data
 
 #%%
-# Checking for duplicates
+# Checking and removing duplicates
 dups = df.duplicated(subset = None, keep = 'first')
 duplicates = dups[dups == True].shape[0]
 print("Number of observations in the data frame: ", len(df))
 print("Number of duplicates: ", str(duplicates))
 df.drop_duplicates(inplace=True)
 print("Number of observations in the dataframe after dropping duplicates: ", len(df))
+
+# %%
+# Removing irrelevant columns
+df.drop(['song_name'], axis=1, inplace=True)
+df.head()
+
+# %%
+# Correlations
+sns.heatmap(df.corr(), vmin=-1, vmax=1, center=0) #, annot = True)
+plt.title("Correlations between columns")
+plt.figure(figsize=[25,25])
+plt.show()
+# %%
+# Pair plots for all columns
+sns.pairplot(df)
+plt.show()
+# %%
