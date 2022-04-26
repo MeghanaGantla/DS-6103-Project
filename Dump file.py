bdbc0b9a601136fdf64422c6fbd00c8b963594cc
MeghanaGantla <meghana_gantla@gwu.edu>
2022-04-25 22:04:11 -0400

    outliers

--- Dump file.py
+++ Dump file.py
@@ -32,3 +32,92 @@ plt.show()
 # Count plots for categorical data
 cf.hist(layout = (3,1), figsize=(25,25))
 plt.show()
+
+#%%
+def get_outlier_counts(df, threshold):
+    df = df.copy()
+    
+    # Get the z-score for specified threshold
+    threshold_z_score = stats.norm.ppf(threshold)
+    
+    # Get the z-scores for each value in df
+    z_score_df = pd.DataFrame(np.abs(stats.zscore(df)), columns=df.columns)
+    
+    # Compare df z_scores to the threshold and return the count of outliers in each column
+    return (z_score_df > threshold_z_score).sum(axis=0)
+
+#%%
+get_outlier_counts(df, 0.99999999999)
+
+#%%
+def remove_outliers(df, threshold):
+    df = df.copy()
+    
+    # Get the z-score for specified threshold
+    threshold_z_score = stats.norm.ppf(threshold)
+    
+    # Get the z-scores for each value in df
+    z_score_df = pd.DataFrame(np.abs(stats.zscore(df)), columns=df.columns)
+    z_score_df = z_score_df > threshold_z_score
+    
+    # Get indices of the outliers
+    outliers = z_score_df.sum(axis=1)
+    outliers = outliers > 0
+    outlier_indices = df.index[outliers]
+    
+    # Drop outlier examples
+    df = df.drop(outlier_indices, axis=0).reset_index(drop=True)
+    
+    return df
+
+#%%
+
+def preprocess_inputs(df, outliers=True, threshold=0.95):
+    df = df.copy()
+    
+    # Remove outliers if specified
+    if outliers == False:
+        df = remove_outliers(df, threshold)
+    
+    # Split df into X and y
+    y = df['popularity'].copy()
+    X = df.drop('popularity', axis=1).copy()
+    
+    # Train-test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=1)
+    
+    # Scale X with a standard scaler
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+    
+    return X_train, X_test, y_train, y_test
+
+#%%
+outlier_X_train, outlier_X_test, outlier_y_train, outlier_y_test = preprocess_inputs(df, outliers=True)
+
+X_train, X_test, y_train, y_test = preprocess_inputs(df, outliers=False, threshold=0.8)
+
+#%%
+# With outliers
+
+outlier_model = LinearRegression()
+outlier_model.fit(outlier_X_train, outlier_y_train)
+
+outlier_model_acc = outlier_model.score(outlier_X_test, outlier_y_test)
+
+print("Test Accuracy (Outliers): {:.5f}%".format(outlier_model_acc * 100))
+
+
+#%%
+# Without outliers
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+model_acc = model.score(X_test, y_test)
+
+print("Test Accuracy (No Outliers): {:.5f}%".format(model_acc * 100))
+# %%
--- EDA and Analysis.py
+++ EDA and Analysis.py
@@ -8,6 +8,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import math
+from scipy import stats
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 #%%
 # Reading data
@@ -16,7 +19,7 @@ df.head()
 
 # %%
 # # Changing column names
-df.rename(columns = {'song_popularity':'popularity', 'song_duration_ms':'duration', 'audio_mode':'mode', 'audio_valence':'valence', 'time_signature':'tsign'}, inplace = True)
+df.rename(columns = {'song_duration_ms':'duration', 'audio_mode':'mode', 'audio_valence':'valence', 'time_signature':'tsign'}, inplace = True)
 df.head()
 #%% 
 # Checking datatypes 
@@ -77,12 +80,20 @@ cf.head()
 
 # %%
 # Histograms for Numerical data
-fig, axis = plt.subplots(5, 2)
-fig.set_size_inches(25, 25)
-column = nf.columns
-for i in range(5):
-    for j in range(2):
-        sns.histplot(nf[column[2*i+j]], ax=axis[i, j], color = list(np.random.randint([255,255,255])/255))
+#fig, axis = plt.subplots(5, 2)
+#fig.set_size_inches(25, 25)
+#column = nf.columns
+#for i in range(5):
+#    for j in range(2):
+#        sns.histplot(nf[column[2*i+j]], ax=axis[i, j], color = list(np.random.randint([255,255,255])/255))
+
+#%%
+# Histograms for Numerical data
+plt.figure(figsize=(25,25))
+for i in range(len(nf.columns)):
+    plt.subplot(5, 2, i+1)
+    sns.histplot(nf[nf.columns[i]], color = list(np.random.randint([255,255,255])/255))
+plt.show()
 
 #%%
 # Count plots for categorical data
@@ -95,6 +106,14 @@ plt.xlabel("Time Signature")
 plt.show()
 
 # %%
+# Boxplots for numerical data
+plt.figure(figsize=(25,25))
+for i in range(len(nf.columns)):
+    plt.subplot(5, 2, i+1)
+    sns.boxplot(nf.columns[i], data = nf, color = list(np.random.randint([255,255,255])/255))
+plt.show()
+
+# we can see quite a few outliers in the data
 # %%
 # Do regresion analysis 
 # FORMULA based
@@ -109,14 +128,14 @@ from sklearn.metrics import classification_report
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsClassifier
 #%%
-model = ols(formula='popularity ~key + mode + tsign+duration + acousticness + danceability + energy + liveness + loudness + speechiness + tempo + valence', data=df)
+model = ols(formula='popularity ~ key + mode + tsign + duration + acousticness + danceability + energy + liveness + loudness + speechiness + tempo + valence', data=df)
 print( type(model) ) 
 
 modelFit = model.fit()
 print( modelFit.summary() )
 #%%
 #Drop variables which P-value <0.05
-model2 = ols(formula='popularity ~mode + tsign + acousticness + danceability + energy + liveness + loudness + speechiness + tempo + valence', data=df)
+model2 = ols(formula='popularity ~ mode + tsign + acousticness + danceability + energy + liveness + loudness + speechiness + tempo + valence', data=df)
 print( type(model2) ) 
 model2Fit = model2.fit()
 print( model2Fit.summary() )
@@ -132,7 +151,8 @@ model = LinearRegression(normalize=True)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 print(np.sum(abs(y_pred - y_test)) / len(y_pred))
-print(model.score(X_test, y_test))
+accu = model.score(X_test, y_test)
+print("blah: {:.5f}%".format(accu * 100))
 
 
 #%%Then bulid tree decision model
@@ -162,3 +182,57 @@ knn.fit
 y_pred = knn.predict(x_popularity )                                                                                                 
 knn.score(x_popularity,y_popularity)
 # Can't not fit, the reason may be the y have too much values and not dummy(catelogy) variable
+
+# %%
+df["popularity"]= [ 1 if i>=66.5 else 0 for i in df.song_popularity ]
+df["popularity"].value_counts()
+df.info()
+
+#%%
+from collections import Counter
+def detect_outliers(df,features):
+    outlier_indices = []
+    
+    for c in features:
+        # 1st quartile
+        Q1 = np.percentile(df[c],25)
+        # 3rd quartile
+        Q3 = np.percentile(df[c],75)
+        # IQR
+        IQR = Q3 - Q1
+        # Outlier step
+        outlier_step = IQR * 1.5
+        # detect outlier and their indeces
+        outlier_list_col = df[(df[c] < Q1 - outlier_step) | (df[c] > Q3 + outlier_step)].index #filtre
+        # store indeces
+        outlier_indices.extend(outlier_list_col) #The extend() extends the list by adding all items of a list (passed as an argument) to the end.
+    
+    outlier_indices = Counter(outlier_indices)
+    multiple_outliers = list(i for i, v in outlier_indices.items() if v > 2) 
+    
+    return multiple_outliers
+# %%
+df.loc[detect_outliers(df,["popularity","duration","danceability","energy","instrumentalness","liveness","loudness","speechiness","valence"])]
+# %%
+song_data = df.drop(detect_outliers(df,["popularity","duration","danceability","energy","liveness","loudness","speechiness","valence"]),axis = 0).reset_index(drop = True)
+
+# %%
+sns.heatmap(song_data.corr(), vmin=-1, vmax=1, center=0)
+plt.show()
+# %%
+y = df['popularity'].copy()
+X = df.drop('popularity', axis=1).copy()
+    
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=1)
+    
+# %%
+# Without outliers
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+model_acc = model.score(X_test, y_test)
+
+print("Test Accuracy (No Outliers): {:.5f}%".format(model_acc * 100))
+# %%
