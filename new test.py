012062f1601fbe7deea87327818deba9cb47ab37
MeghanaGantla <meghana_gantla@gwu.edu>
2022-04-26 23:28:08 -0400

    Update new test.py

--- new test.py
+++ new test.py
@@ -32,7 +32,8 @@ df.head()
 #%%
 # Adding a new column
 print(df['song_popularity'].mean())
-df["popularity"]= [2 if i >= 70 else 1 if (i>=50) & (i<=69) else 0 for i in df.song_popularity ]
+df["popularity"]= [1 if i >= 50 else 0 for i in df.song_popularity ]
+#df["popularity"]= [2 if i >= 70 else 1 if (i>=50) & (i<=69) else 0 for i in df.song_popularity ]
 print(df["popularity"].value_counts())
 
 # %%
@@ -64,43 +65,54 @@ plt.show()
 nf = df.drop(["key", "mode", "tsign", "popularity"], axis = 1)
 nf.head()
 
-#%% 
-# Seperating Categorical data
-catf = df[["key", "mode", "tsign", "popularity"]]
-catf.head()
-
 #%%
-cf = df[["key", "mode", "tsign"]]
+# Seperating categorical features
+cf = df[["key", "mode", "tsign", 'popularity']]
 cf.head()
 
 #%%
-#Converting categorical Columns to Numeric
-nvc = pd.DataFrame(df.isnull().sum().sort_values(), columns=['Total Null Values'])
-nvc['Percentage'] = round(nvc['Total Null Values']/df.shape[0],3)*100
-print(nvc)
+# Converting categorical Columns to Numeric
+nulls = pd.DataFrame(df.isnull().sum().sort_values(), columns=['null_values'])
+print(nulls)
+dupdf = df.copy()
+e = nulls[nulls['null_values']!=0].index.values
+f = [i for i in cf if i not in e]
 
-df3 = df.copy()
-
-ecc = nvc[nvc['Percentage']!=0].index.values
-fcc = [i for i in cf if i not in ecc]
-#One-Hot Binay Encoding
+# One-Hot Binay Encoder
 oh=True
 dm=True
-for i in fcc:
-    #print(i)
-    if df3[i].nunique()==2:
-        if oh==True: print("\033[1mOne-Hot Encoding on features:\033[0m")
-        print(i);oh=False
-        df3[i]=pd.get_dummies(df3[i], drop_first=True, prefix=str(i))
-    if (df3[i].nunique()>2 and df3[i].nunique()<17):
-        if dm==True: print("\n\033[1mDummy Encoding on features:\033[0m")
-        print(i);dm=False
-        df3 = pd.concat([df3.drop([i], axis=1), pd.DataFrame(pd.get_dummies(df3[i], drop_first=True, prefix=str(i)))],axis=1)
-        
-df3.head()
+for i in f:
+    if dupdf[i].nunique() == 2:
+        if oh==True: 
+            print("One-Hot Encoding on features:")
+        print(i) 
+        oh=False
+        dupdf[i]=pd.get_dummies(dupdf[i], drop_first=True, prefix=str(i))
+    if (dupdf[i].nunique() > 2 and dupdf[i].nunique() < 17):
+        if dm==True: 
+            print("Dummy Encoding on features:")
+        print(i)
+        dm=False
+        dupdf = pd.concat([dupdf.drop([i], axis=1), pd.DataFrame(pd.get_dummies(dupdf[i], drop_first=True, prefix=str(i)))],axis=1)     
+dupdf.head()
+
+#%%
+# Label Encoder
+from sklearn import preprocessing
+dupdf = df.copy()
+encoder = preprocessing.LabelEncoder()
+dupdf["key"] = encoder.fit_transform(dupdf["key"])
+encoder = preprocessing.LabelEncoder()
+dupdf["mode"] = encoder.fit_transform(dupdf["mode"])
+encoder = preprocessing.LabelEncoder()
+dupdf["tsign"] = encoder.fit_transform(dupdf["tsign"])
+encoder = preprocessing.LabelEncoder()
+dupdf["popularity"] = encoder.fit_transform(dupdf["popularity"])
+dupdf.head()
 
 # %% Removing outliers
-df1 = df3.copy()
+
+df1 = df.copy()
 features1 = nf
 
 for i in features1:
@@ -110,32 +122,32 @@ for i in features1:
     df1 = df1[df1[i] <= (Q3+(1.5*IQR))]
     df1 = df1[df1[i] >= (Q1-(1.5*IQR))]
     df1 = df1.reset_index(drop=True)
-print('\n\033[1mInference:\033[0m\nBefore removal of outliers, The dataset had {} samples.'.format(df3.shape[0]))
+print('\n\033[1mInference:\033[0m\nBefore removal of outliers, The dataset had {} samples.'.format(dupdf.shape[0]))
 print('After removal of outliers, The dataset now has {} samples.'.format(df1.shape[0]))
 df1.head()
 # %%
+# Splitting data
 y = df1['popularity']
-X = df1.drop('popularity', axis=1).copy()
-    
+X = df1.drop('popularity', axis=1).copy()    
 # Train-test split
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=1)
     
 # %%
+# Linear regression model
 model = LinearRegression()
 model.fit(X_train, y_train)
-
 model_acc = model.score(X_test, y_test)
-
 print("Test Accuracy {:.5f}%".format(model_acc * 100))
-# %%
 
 #%%
-model = ols(formula='popularity ~ key + mode + tsign + duration + acousticness + danceability + energy + liveness + loudness + speechiness + tempo + valence', data=df)
+# Logit model 1
+model = ols(formula='popularity ~ key + mode + tsign + duration + acousticness + danceability + energy + liveness + loudness + speechiness + tempo + valence + instrumentalness', data=df)
 print(type(model)) 
 modelFit = model.fit()
 print(modelFit.summary())
 
 #%%
+# Logit model 2
 #Drop variables which P-value <0.05
 model2 = ols(formula='popularity ~ mode + tsign + acousticness + danceability + energy + liveness + loudness + speechiness + tempo + valence', data=df)
 print(type(model2) ) 
@@ -144,43 +156,45 @@ print(model2Fit.summary())
 
 #%%
 #Build linear regression model
-df["popularity"].where(df["popularity"] < 99, 99, True)
-df['popularity'].value_counts()
-
+#df["popularity"].where(df["popularity"] < 99, 99, True)
+print(df['popularity'].value_counts())
 x_popularity = df[['key', 'mode', 'tsign', 'duration', 'acousticness', 'danceability', 'energy', 'liveness', 'loudness', 'speechiness', 'tempo', 'valence']]
 y_popularity = df['popularity']
-X_train, X_test, y_train, y_test= train_test_split(x_popularity, y_popularity, test_size=0.2, stratify=y_popularity,random_state=1)
+X_train, X_test, y_train, y_test= train_test_split(x_popularity, y_popularity, test_size=0.2, stratify=y_popularity, random_state=1)
+
+#%%
+# Linear regression model 2
 model = LinearRegression(normalize=True)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
-print(np.sum(abs(y_pred - y_test)) / len(y_pred))
+print(np.sum(abs(y_pred - y_test))/len(y_pred))
 accu = model.score(X_test, y_test)
-print("blah: {:.5f}%".format(accu * 100))
-
+print("Accuracy: {:.5f}%".format(accu * 100))
 
 #%%Then bulid tree decision model
-
-# %%
 #Error: The least populated class in y has only 1 member, which is too few. The minimum number of groups for any class cannot be less than 2.
 #So need change the part of dataframe
 #df['popularity'].value_counts()
-x_popularity = df[['key', 'mode', 'tsign', 'duration', 'acousticness', 'danceability', 'energy', 'liveness', 'loudness', 'speechiness', 'tempo', 'valence']]
-y_popularity = df['popularity']
-df["popularity"].where(df["popularity"] < 99, 99, True)
-df['popularity'].value_counts()
-X_train, X_test, y_train, y_test= train_test_split(x_popularity, y_popularity, test_size=0.2, stratify=y_popularity,random_state=1)
+#x_popularity = df[['key', 'mode', 'tsign', 'duration', 'acousticness', 'danceability', 'energy', 'liveness', 'loudness', 'speechiness', 'tempo', 'valence']]
+#y_popularity = df['popularity']
+#df["popularity"].where(df["popularity"] < 99, 99, True)
+#df['popularity'].value_counts()
+#X_train, X_test, y_train, y_test= train_test_split(x_popularity, y_popularity, test_size=0.2, stratify=y_popularity,random_state=1)
 #%%
+# Decision tree
 dtree_popularity1 = DecisionTreeClassifier( random_state=1)
 dtree_popularity1.fit(X_train,y_train)
 y_test_pred = dtree_popularity1.predict(X_test)
-#%%
 print(accuracy_score(y_test, y_test_pred))
-# print(confusion_matrix(y_test, y_test_pred))
-# print(classification_report(y_test, y_test_pred))
+print(confusion_matrix(y_test, y_test_pred))
+print(classification_report(y_test, y_test_pred))
+
 # %%
-#Then try knn model 
+# KNN model 
 from sklearn.neighbors import KNeighborsClassifier
 knn = KNeighborsClassifier(n_neighbors = 10)
 knn.fit(X_train, y_train)
 y_pred = knn.predict(x_popularity )                                                                                                 
-knn.score(x_popularity,y_popularity)
+knn.score(x_popularity, y_popularity)
+
+# %%
