db68d545557d978df1b473da1b7ef917ad1f357c
ADA21-K <adarshkotla@gwu.edu>
2022-05-06 09:55:31 -0400

    Update Dtree_a.py

--- Dtree_a.py
+++ Dtree_a.py
@@ -1,11 +1,29 @@
 # %%
 # Dtree models
+
 # %%
 # importing necessary libraries
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import datasets
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
+
+#%%
+# Reading data
+df = pd.read_csv("SpotifyAudioFeaturesNov2018.csv")
+df.head()
+
+# %%
+# # Changing column names
+df.rename(columns = {'duration_ms':'duration', 'time_signature':'tsign', 'popularity':'song_popularity'}, inplace = True)
+df.head()
+
+#%%
+# Adding a new column
+print(df['song_popularity'].mean())
+df["popularity"]= [1 if i >= 50 else 0 for i in df.song_popularity ]
+#df["popularity"]= [2 if i >= 70 else 1 if (i>=50) & (i<=69) else 0 for i in df.song_popularity ]
+print(df["popularity"].value_counts())
 
 # loading the iris dataset
 iris = datasets.load_iris()
